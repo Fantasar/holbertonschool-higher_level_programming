61b6ef009fcc2eec1e9c96c8cbfcd01faa568488
Fantasar <lapique.philippe@gmail.com>
2025-09-05 11:26:27 +0200

    Modification du pycodestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 from sys import argv
 if __name__ == "__main__":
     count = len(argv) - 1
-    if count == 0: 
+    if count == 0:
         print("{} arguments.".format(count))
     elif count > 1:
         print("{} arguments:".format(count))
-        for i in range (1, count + 1):
+        for i in range(1, count + 1):
             print("{}: {}".format(i, argv[i]))
     else:
         print("{} argument:".format(count))
