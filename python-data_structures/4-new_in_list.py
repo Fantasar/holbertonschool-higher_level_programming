55819f4ca26eee0c14e5f37612c54c37632ec38b
Fantasar <lapique.philippe@gmail.com>
2025-09-08 11:46:09 +0200

    Ajout des fichiers pour l'exercices 4 : Remplacer un éléments d'un idex donner sur une copie de chaine

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    longeur = len(my_list)
+    if idx < 0:
+        return my_list
+    elif idx > longeur:
+        return my_list
+    else:
+        copy_my_list = my_list.copy()
+        copy_my_list[idx] = element
+        return copy_my_list
