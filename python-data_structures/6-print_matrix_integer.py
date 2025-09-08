dc1c8b71a9071471993a2caba1daf74761242b8c
Fantasar <lapique.philippe@gmail.com>
2025-09-08 12:41:12 +0200

    Ajout des fichiers pour l'exercice 6 : afficher une structure de matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    longeur = len(matrix)
+    index = 0
+    while index < longeur:
+        largeur = len(matrix[index])
+        position = 0
+
+        while position < largeur:
+            print("{}".format(matrix[index][position]), end=" ")
+            position += 1
+
+        print("")
+        index += 1
