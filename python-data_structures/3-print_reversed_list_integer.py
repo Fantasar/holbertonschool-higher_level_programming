f7981837b4eb65f8f6e3feb5d850dbc5df288007
Fantasar <lapique.philippe@gmail.com>
2025-09-08 13:19:59 +0200

    Modification des decimals

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -5,5 +5,5 @@ def print_reversed_list_integer(my_list=[]):
     a = 0
     longeur = len(my_list)
     while a < longeur:
-        print("{}".format(my_list[a]))
+        print("{:d}".format(my_list[a]))
         a += 1
--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -9,3 +9,4 @@ def new_in_list(my_list, idx, element):
         copy_my_list = my_list.copy()
         copy_my_list[idx] = element
         return copy_my_list
+    
