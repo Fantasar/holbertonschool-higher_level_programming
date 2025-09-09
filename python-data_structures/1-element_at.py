36d2549a566a24109cbddb1a1f9f8a49213a9914
Fantasar <lapique.philippe@gmail.com>
2025-09-09 09:49:31 +0200

    Modification des conditions d'affectation >= pour le checker

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,7 +4,7 @@ def element_at(my_list, idx):
     longeur = len(my_list)
     if idx < 0:
         return None
-    elif idx > longeur:
+    elif idx >= longeur:
         return None
     else:
         return my_list[idx]
