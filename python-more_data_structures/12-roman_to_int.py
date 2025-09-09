29e8f2da22e767536a42801d58295b150108217e
Fantasar <lapique.philippe@gmail.com>
2025-09-09 17:34:23 +0200

    Ajout du brouillon pour l'exercice 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is not str:
+        return None 
+    if roman_string == None:
+        return None
+    new_dict = {'I' = 1, 'II' = 2, 'III' = 3, 'IV' = 4,
+    'X'= 5}
+
+    roman_string = new_dict[values]
+    return roman_string
+                
