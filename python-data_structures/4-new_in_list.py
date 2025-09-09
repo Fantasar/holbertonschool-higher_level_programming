14c4b7b30b2033c29e532db5f6e3ba754e29be36
Fantasar <lapique.philippe@gmail.com>
2025-09-09 09:54:57 +0200

    Modification des condition d'affectation ainsi que des valeurs de retours pour elif

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,8 +3,8 @@ def new_in_list(my_list, idx, element):
     longeur = len(my_list)
     if idx < 0:
         return my_list
-    elif idx > longeur:
-        return my_list
+    elif idx >= longeur:
+        return my_list.copy()
     else:
         copy_my_list = my_list.copy()
         copy_my_list[idx] = element
