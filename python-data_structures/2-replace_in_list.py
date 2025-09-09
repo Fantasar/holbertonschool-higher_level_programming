e83aca116c798b069e9eff66997d20d3f911bcf3
Fantasar <lapique.philippe@gmail.com>
2025-09-09 09:52:04 +0200

    Modification pour la correction des opérateur de logique -> >=

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,8 +3,8 @@ def replace_in_list(my_list, idx, element):
     longeur = len(my_list)
     if idx < 0:
         return None
-    elif idx > longeur:
-        return None
+    elif idx >= longeur:
+        return my_list
     else:
         my_list[idx] = element
         return my_list
