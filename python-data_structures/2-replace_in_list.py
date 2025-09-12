0c9ccd7c104e455a6505e92a68db80c0a1431a12
Fantasar <lapique.philippe@gmail.com>
2025-09-12 14:05:52 +0200

    Modif pycodestyle

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     longeur = len(my_list)
     if idx < 0:
-        return None
+        return my_list
     elif idx >= longeur:
         return my_list
     else:
