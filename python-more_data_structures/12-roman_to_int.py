8d1f7e1bd17281cef03bb417277a03ffd62c9b4e
Fantasar <lapique.philippe@gmail.com>
2025-09-13 23:05:45 +0200

    Modification des conditions pour les boulces if

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is not str:
-        return None 
-    if roman_string == None:
-        return None
-    new_dict = {'I' = 1, 'II' = 2, 'III' = 3, 'IV' = 4,
-    'X'= 5}
 
-    roman_string = new_dict[values]
-    return roman_string
-                
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    new_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    values = 0
+    idx = 0
+
+    while idx < len(roman_string):
+        prev = new_dict.get(roman_string[idx], 0)
+
+        if idx + 1 < len(roman_string):
+            next_value = new_dict.get(roman_string[idx + 1], 0)
+
+            if prev < next_value:
+                values += next_value - prev
+                idx += 2
+                continue
+
+        values += prev
+        idx += 1
+
+    return values
