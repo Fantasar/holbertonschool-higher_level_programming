bb4909834c5db768fb013fe85e0e902978bc0725
Fantasar <lapique.philippe@gmail.com>
2025-09-15 15:17:57 +0200

    Ajouts des fichiers pour l'exercice 1: Créer un carré de longeur size

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Define modules Square size
+"""
+
+
+class Square:
+    """
+    define Square classe.
+    """
+    def __init__(self, values):
+        """
+        define function args :
+            values is size of square
+        """
+        self._Square__size = values
