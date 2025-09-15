70e3604695eeefac4ae8a8d18e4d41cb193e7f6e
Fantasar <lapique.philippe@gmail.com>
2025-09-15 15:54:05 +0200

    Ajout des fichiers pour l'exercice 2: Contient les modules pour contrôler la taille de square

---
+++ python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Is modules for print géométrique cube.
+"""
+
+
+class Square:
+    """
+    Square is class for cube.
+    """
+    def __init__(self, size=0):
+        """
+        Is function for controls size of square.
+
+        Args:
+        size: is int of longs square
+
+        Raise:
+        TypeError: is not type int for size.
+        ValueError: size is > 0.
+        """
+
+        self._Square__size = size
+
+        if not isinstance(self._Square__size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
