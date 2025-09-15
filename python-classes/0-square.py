f3b87048f268b00321db6a2ce6b8c2fdae4a74d8
Fantasar <lapique.philippe@gmail.com>
2025-09-15 14:15:26 +0200

    Ajout du dossier et de l'exercice 0 pour les class et les objets en python

---
+++ python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/sr/bin/python3
+"""
+This moduls define a square ans prints.
+"""
+
+
+class Square:
+    """
+    class define a square.
+    """
+    def __init__(self):
+        """
+        continue the function
+        """
+        pass
