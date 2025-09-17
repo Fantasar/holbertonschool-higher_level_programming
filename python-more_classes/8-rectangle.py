9bd4f49b84239eac2e5a36055ce4418295089e10
Fantasar <lapique.philippe@gmail.com>
2025-09-17 13:59:08 +0200

    Ajout des fichiers pour l'exercice 8 :Utilisation d'une methode statique

---
+++ python-more_classes/8-rectangle.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python3
+"""
+Write and define a Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    Represent class Rectangle
+    """
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """
+        methods for attribut width and height.
+        """
+        Rectangle.number_of_instances += 1
+        self._Rectangle__width = width
+        self._Rectangle__height = height
+
+    @property
+    def width(self):
+        """
+        Method for return width rectangle
+        """
+        return self._Rectangle__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Methods for controls and attribute width values
+
+        Raise:
+        TypeError : Is not int values
+        ValueError : Is < at 0
+
+        """
+        if not isinstance(self._Rectangle__width, int):
+            raise TypeError("width must be an integer")
+        if self._Rectangle__width < 0:
+            raise ValueError("width must be >= 0")
+
+        self._Rectangle__width = value
+
+    @property
+    def height(self):
+        """
+        Methods for return height rectangle
+        """
+        return self._Rectangle__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Methods for controls and attribut values of height rectangle
+
+        Raise:
+        TypeError : Is not int values
+        ValueError : Is < at 0
+
+        """
+        if not isinstance(self._Rectangle__height, int):
+            raise TypeError("height must be an integer")
+        if self._Rectangle__height < 0:
+            raise ValueError("height must be >= 0")
+
+        self._Rectangle__height = value
+
+    def area(self):
+        """
+        Methode instance for calculs area
+        """
+        return self._Rectangle__height * self._Rectangle__width
+
+    def perimeter(self):
+        """
+        Methode instance for calculs perimeter
+        """
+        if self._Rectangle__height == 0 or self._Rectangle__width == 0:
+            return 0
+        return 2 * (self._Rectangle__height + self._Rectangle__width)
+
+    def __str__(self):
+        """
+        Methode for print a rectangle with str
+        """
+        if self.height == 0 or self.width == 0:
+            return ""
+
+        chaine = ""
+        for idx in range(self.height):
+            chaine += str(self.print_symbol) * self.width
+            if idx != self.height - 1:
+                chaine += "\n"
+        return chaine
+
+    def __repr__(self):
+        """
+        Methode for print object
+        """
+        return ("Rectangle({}, {})".format
+                (self._Rectangle__width, self._Rectangle__height))
+
+    def __del__(self):
+        """
+        Methode for delete instance rectangle
+        """
+        if not Rectangle.number_of_instances <= 0:
+            Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """
+        Methode static for controle value of rect-1 and rect-2
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() == rect_2.area():
+            return rect_1
+        if rect_1.area() < rect_2.area():
+            return rect_2
+        if rect_1.area() > rect_2.area():
+            return rect_1
