f53c846e3c67538616c8d9dba8b82196896e4ad4
Fantasar <lapique.philippe@gmail.com>
2025-09-22 16:34:29 +0200

    Ajout des fichiers pour l'exercices 7: Intégration d'une méthode pour contrôler les valeurs avec leur type et si elle sont supérieur a 0

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+Module pour contrôler une forme de géométrie de base
+"""
+
+
+class BaseGeometry:
+
+    """
+    Création de la class BaseGeometry
+    """
+
+    def area(self):
+
+        """
+        Méthode pour contrôler l'air d'une forme géométrique;
+
+        Raise:
+        Exception lorsequ'il n'est pas possible de le calculer
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+
+        """
+        Methode pour intégrer une valeur.
+
+        Raise:
+        TypeError : La valeur n'est pas un entier.
+        ValueError : La valeur est inférieur à 0.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
