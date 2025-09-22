31b754c20bfa8f6c779c77e30b70924a54058c8d
Fantasar <lapique.philippe@gmail.com>
2025-09-22 16:49:59 +0200

    Ajout des fichiers pou l'exercices 8 : Création de la classe fille rectangle et de sa méthode

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+"""
+Module pour contrôler une forme de géométrie de base
+"""
+
+
+class BaseGeometry:
+
+    """
+    Création de la class BaseGeometry
+    """
+
+    def area(self):
+
+        """
+        Méthode pour contrôler l'air d'une forme géométrique;
+
+        Raise:
+        Exception lorsequ'il n'est pas possible de le calculer
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+
+        """
+        Methode pour intégrer une valeur.
+
+        Raise:
+        TypeError : La valeur n'est pas un entier.
+        ValueError : La valeur est inférieur à 0.
+        """
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+
+    """
+    Création d'une classe fille de BaseGeometry.
+    """
+
+    def __init__(self, width, height):
+
+        """
+        Initialisation d'une méthode qui prends en paramètre
+        la largeur et la hauteur.
+        """
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
