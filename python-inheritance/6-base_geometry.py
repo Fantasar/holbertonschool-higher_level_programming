d0466c309327f1308e13384cb65fa990dc22f5c0
Fantasar <lapique.philippe@gmail.com>
2025-09-22 16:10:30 +0200

    Ajout des fichiers pour l'exercice 6 : Intégration d'un cas d'erreur dans la class BaseGeometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Module pour contrôler une forme de géométrie de base
+"""
+
+
+class BaseGeometry:
+
+    """
+    Création de la class BaseGeometry
+    """
+
+    def area(self):
+
+        """
+        Méthode pour contrôler l'air d'une forme géométrique;
+
+        Raise:
+        Exception lorsequ'il n'est pas possible de le calculer
+        """
+
+        raise Exception("area() is not implemented")
