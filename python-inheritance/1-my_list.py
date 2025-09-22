e444e8e2c8919f1ffecfbf8c216293eec2d1dead
Fantasar <lapique.philippe@gmail.com>
2025-09-22 14:57:12 +0200

    Ajout des fichiers pour l'exercice 1 : Utilisation de la commande sorted pour trier la list et en créer une copie

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module pour construire une classe ansi que sa fille
+"""
+
+
+class MyList(list):
+    """
+    Construction de la class MyList
+    """
+    def print_sorted(self):
+        """
+        Méthodes d'instance qui imprime une listes triés
+        """
+        print(sorted(self))
