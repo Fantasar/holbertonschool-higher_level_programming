eb09f882a0c6ba0d2e592c180af2f37b3393a168
Fantasar <lapique.philippe@gmail.com>
2025-09-29 15:33:27 +0200

    Ajout des fichiers de l'exercices 3: Utilisation de la méthodes JSON pour convertir un objet en string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import json
+
+
+"""
+Module qui permet de travailler sur les intéractions avec l'utilisateur,
+en utilisant les "Input" et des "Output"
+"""
+
+
+def to_json_string(my_obj):
+
+    """
+    Méthode d'instance pour utiliser la représentation d'un objet
+    dans le format JSON.
+    """
+
+    conteneur = json.dumps(my_obj)
+    print("{}".format(conteneur))
