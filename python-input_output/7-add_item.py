b3edefab3692d73a30839232c56ca2cb31ed219d
Fantasar <lapique.philippe@gmail.com>
2025-09-29 17:54:57 +0200

    Ajout du script pour l'exercice 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Module qui permet de travailler sur les intéractions avec l'utilisateur,
+en utilisant les "Input" et des "Output"
+"""
+
+import json
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
