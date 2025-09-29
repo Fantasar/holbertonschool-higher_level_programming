4d32cc099736526d25461ecd8bbf1aaf871d1e96
Fantasar <lapique.philippe@gmail.com>
2025-09-29 15:40:05 +0200

    Ajout des fichiers pour l'exercice 4 : Utilisation de la méthodes de json.loads pour convertir une string en objet

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import json
+
+"""
+Module qui permet de travailler sur les intéractions avec l'utilisateur,
+en utilisant les "Input" et des "Output"
+"""
+
+
+def from_json_string(my_str):
+
+    """
+    Méthode d'instance qui retourne un objet représenter en json string.
+    """
+
+    conteneur = json.loads(my_str)
+    print("{}".format(conteneur))
