ab794aac97e3b5b04f4149ae0b1f4422cf6e4c34
Fantasar <lapique.philippe@gmail.com>
2025-10-09 19:12:33 +0200

    Modification du pycodestyle pour la exercice 04

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""
+Module qui permet de simuler un serveur et de gérer plusieurs commandes
+comme l'ajout d'un utilisateur dans un dictionnaire
+"""
+
 from flask import Flask
 from flask import jsonify
 import json
@@ -11,32 +16,61 @@ users = {
     "jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}
 }
 
+
 @app.route("/")
-def home ():
+def home():
+    """
+    Méthode instance pour initialiser la page de garde
+    """
+
     return "<p>Welcome to the Flask API!</p>"
+
 
 @app.route("/data")
 def get_users():
+    """
+    Méthode d'instance pour afficher les utilisateurs de la
+    base de donnée
+    """
+
     return jsonify(users), 200
+
 
 @app.route("/status")
 def status():
+    """
+    Méthode d'instance pour indiquer l'état de la page web
+    """
     return "<p>OK</p>"
+
 
 @app.route("/users/<username>")
 def get_user(username):
+    """
+    Méthode pour recherche un utilisateur particulier
+    dans la base de donnée
+    """
+
     user = users.get(username)
     if user:
         return jsonify(user), 200
     else:
-        return ({"Error" :"User not found"}), 404
+        return ({"Error": "User not found"}), 404
+
 
 @app.route("/add_user/<username>")
 def add_user(username):
+    """
+    Méthode pour ajouter un utilisateur dans la base de donnée
+    """
+
     if username in users:
-        return jsonify({"Error" : "User alredy exists"}), 400
+        return jsonify({"Error": "User alredy exists"}), 400
     users[username] = {"name": username, "age": None, "city": None}
 
-    return jsonify({"message": f"User '{username}' added successfully", "users": users}), 201
+    return jsonify({"message": f"User '{username}' added successfully",
+                    "users": users}), 201
 
-if __name__ == "__main__": app.run()
+
+if __name__ == "__main__":
+    app.run()
