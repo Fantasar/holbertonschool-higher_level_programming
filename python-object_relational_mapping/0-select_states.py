f7b07e17665cfaefd8d3ae67cdadfa4458a01f62
Fantasar <lapique.philippe@gmail.com>
2025-10-23 17:49:30 +0200

    Modification du pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,16 +10,15 @@ import sys
 
 
 if __name__ == "__main__":
-
     username = sys.argv[1]
-    passworld = sys.argv[2]
-    database = sys.argv[3]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
     db = MySQLdb.connect(
-        host='localhost',
+        host="localhost",
         port=3306,
         user=username,
-        passwd=passworld,
+        passwd=password,
         db=db_name
     )
 
