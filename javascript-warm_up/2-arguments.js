5566621e7b5f14a58b80bfbd1e91304496cbb69f
Fantasar <lapique.philippe@gmail.com>
2025-11-07 18:07:07 +0100

    semistandar

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const args = process.argv.length;
 
 if (args === 2) {
-    console.log('No argument');
-} else if (arg === 3) {
-    console.log('Argument found')
+  console.log('No argument');
+} else if (args === 3) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found')
+  console.log('Arguments found');
 }
