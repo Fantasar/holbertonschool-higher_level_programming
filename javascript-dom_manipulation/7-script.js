1e28ce42fbbd47e04813e8820962541dfd94a361
Fantasar <lapique.philippe@gmail.com>
2025-11-14 13:06:53 +0100

    Modification du fichier

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,14 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-  fetch('https://swapi-api.hbtn.io/api/films/?format=json')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.results);
-      const listMovies = document.getElementById('listMovies');
-      data.results.forEach(movie => {
-        const li = document.createElement('li');
-        li.innerText = movie.title;
-        listMovies.appendChild(li);
-      });
-    })
-    .catch(error => console.error('Erreur:', error));
-});
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const listMovies = document.getElementById('list_movies');
+    data.results.forEach(movie => {
+      const li = document.createElement('li');
+      li.textContent = movie.title;
+      listMovies.appendChild(li);
+    });
+  })
+  .catch(error => console.error('Error:', error));
